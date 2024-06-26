ea2a125e1b51365e4d35b3d67aaa7e8a8f146e7d
h4lllllll <hjod1172@yahoo.co.jp>
2024-06-26 11:45:50 +0000

    f

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,52 +1,46 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { Box } from "@chakra-ui/react";
-import type { FC } from "react";
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { Box } from '@chakra-ui/react';
+import type { FC } from 'react';
 
 const Component: FC = () => {
-	return (
-		<>
-			<Box>yes?</Box>
-			<Link to="/user">{">aaa<"}</Link>
-		</>
-	);
+  return (
+    <>
+      <Box>yes?</Box>
+      <Link to="/user">{'>aaa<'}</Link>
+    </>
+  );
 };
 
-export const Route = createFileRoute("/")({
-	component: Component,
-	loader: async ({ context }) => {
-		// routerとqueryのテスト
-		// 画面に関わることなのでcomponentにしようと思ったが、結果が一つの場合で分けたいので
-		// get user msal data
-		const { userId, IdToken } = context.azAuth;
-		console.log(`login user? ${userId}`);
-		console.log(`token? ${IdToken}`);
+export const Route = createFileRoute('/')({
+  component: Component,
+  loader: async ({ context }) => {
+    // routerとqueryのテスト
+    // 画面に関わることなのでcomponentにしようと思ったが、結果が一つの場合で分けたいので
+    // get user msal data
+    const { userId, IdToken } = context.azAuth;
 
-		// query client
-		const queryClient = context.queryClient;
-		const data = await queryClient.fetchQuery({
-			queryKey: [userId],
-			queryFn: async () => {
-				const res = await fetch("/api/info", {
-					method: "GET",
-					headers: {
-						Authorization: `Bearer ${IdToken}`,
-					},
-				});
-				// console.log(`idtoken: ${idToken}`);
-				// const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    // query client
+    const queryClient = context.queryClient;
+    const data = await queryClient.fetchQuery({
+      queryKey: [userId],
+      queryFn: async () => {
+        const res = await fetch('/api/info', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${IdToken}`,
+          },
+        });
 
-				if (!res.ok) {
-					const err = await res.json();
-					console.error(`エラーだよ！ ${JSON.stringify(err)}`);
-					throw new Error(err);
-				}
-				return await res.json();
-			},
-		});
-		console.log("ok???");
-		console.log(data);
-	},
-	pendingComponent: () => {
-		return <>{"waiting..."}</>;
-	},
+        if (!res.ok) {
+          const err = await res.json();
+          throw new Error(err);
+        }
+        return await res.json();
+      },
+    });
+    console.log(data);
+  },
+  pendingComponent: () => {
+    return <>{'waiting...'}</>;
+  },
 });
